63b5e032f41b44e93f1268ac681b43e2bd60ebcb
demetre1243 <gagiladze.d@gmail.com>
2026-02-15 23:19:07 +0400

    addition

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Happy developing ✨')
+//--------------n1-------------//
+
+/*let count = 0;
+const countDiv = document.getElementById("count");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+plusBtn.addEventListener("click", function () {
+    count++;
+    countDiv.textContent = count;
+});
+minusBtn.addEventListener("click", function () {
+    count--;
+    if(count<0){
+        count = 0;
+    }
+    countDiv.textContent = count;
+});
+ */
+
+
